d781dfa03b28e93fb98d541dda04a3741bffe659
kdilot <kdilot@gmail.com>
2021-01-16 11:22:31 +0900

    [UPDATE] screen size

--- components/wrapper/index.tsx
+++ components/wrapper/index.tsx
@@ -1,15 +1,10 @@
 import styled from 'styled-components';
 import Header from 'components/header';
-import { useMediaQuery } from 'react-responsive';
+import { useScroll } from 'hooks/common';
+import { useEffect } from 'react';
 
 const Wrapper: React.FC = ({ children }) => {
-    const isMobile = useMediaQuery({ maxWidth: 414 });
-    return isMobile ? (
-        <MobileContainer>
-            <Header />
-            <ChildContainer>{children}</ChildContainer>
-        </MobileContainer>
-    ) : (
+    return (
         <Container>
             <Header />
             <ChildContainer>{children}</ChildContainer>
@@ -19,32 +14,34 @@ const Wrapper: React.FC = ({ children }) => {
 
 export default Wrapper;
 
-const MobileContainer = styled.section`
-    width: 100vw;
-    height: 100vh;
-    overflow: auto;
-    padding-bottom: 10px;
-`;
-
 const Container = styled.section`
-    width: 500px;
-    height: 100vh;
-    overflow: auto;
-    border-left: 1px solid rgba(0, 0, 0, 0.1);
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
-    padding-bottom: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    &::-webkit-scrollbar {
-        display: none;
+    @media screen and (min-width: 414px) {
+        width: 500px;
+        height: 100vh;
+        overflow: auto;
+        border-left: 1px solid rgba(0, 0, 0, 0.1);
+        border-right: 1px solid rgba(0, 0, 0, 0.1);
+        padding-bottom: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+        -moz-box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
+            -3px 0 3px -3px rgba(0, 0, 0, 0.1);
+        -webkit-box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
+            -3px 0 3px -3px rgba(0, 0, 0, 0.1);
+        box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
+            -3px 0 3px -3px rgba(0, 0, 0, 0.1);
     }
-    -moz-box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
-        -3px 0 3px -3px rgba(0, 0, 0, 0.1);
-    -webkit-box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
-        -3px 0 3px -3px rgba(0, 0, 0, 0.1);
-    box-shadow: 3px 0 3px -3px rgba(0, 0, 0, 0.1),
-        -3px 0 3px -3px rgba(0, 0, 0, 0.1);
+
+    @media screen and (max-width: 414px) {
+        width: 100vw;
+        height: 100vh;
+        overflow: auto;
+        padding-bottom: 10px;
+    }
 `;
 
 const ChildContainer = styled.div`
---
+++ hooks/common.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useMemo, useState } from 'react';
+
+export const useScroll = () => {
+    const [scrollTop, setScrollTop] = useState<number>(0);
+    const [scrollBottom, setScrollBottom] = useState<number>(0);
+    const [scrollHeight, setScrollHeight] = useState<number>(0);
+    const [innerHeight, setInnerHeight] = useState<number>(0);
+    const [prevScroll, setPrevScroll] = useState<number>(0);
+    const [isBottom, setIsBottom] = useState<boolean>(false);
+    const isScrollEnd = useMemo(
+        () =>
+            scrollHeight !== 0 && scrollHeight - innerHeight - scrollTop < 400,
+        [scrollHeight, innerHeight, scrollTop],
+    );
+
+    const onScroll = () => {
+        console.log('!!!!');
+        setInnerHeight(window.innerHeight);
+        setScrollHeight(document.body.scrollHeight);
+        setPrevScroll(scrollTop);
+        const top =
+            window.scrollY ||
+            (document.documentElement && document.documentElement.scrollTop) ||
+            document.body.scrollTop;
+        setScrollTop(top);
+
+        const docHeight = Math.max(
+            document.body.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight,
+            document.documentElement.offsetHeight,
+        );
+        const bottom = window.innerHeight + window.pageYOffset;
+
+        setScrollBottom(bottom);
+        setIsBottom(bottom >= docHeight ? true : false);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll, false);
+
+        return () => {
+            window.removeEventListener('scroll', onScroll, false);
+        };
+    });
+
+    return {
+        scrollTop,
+        scrollBottom,
+        scrollHeight,
+        innerHeight,
+        prevScroll,
+        isScrollEnd,
+        isBottom,
+    };
+};
