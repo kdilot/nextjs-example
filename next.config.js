59e1fcbd6375e8bbec2f2aff8bf7f0731cae5b13
kdilot <kdilotdev@gmail.com>
2021-02-06 15:57:55 +0900

    [UPDATE] route, package

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     async redirects() {
         return [
-            {
-                source: '/naver',
-                destination: 'https://www.naver.com/',
-                permanent: false,
-            },
-        ];
+            // {
+            //     source: '/',
+            //     destination: '/post',
+            //     permanent: false,
+            // },
+        ]
     },
-};
+}
